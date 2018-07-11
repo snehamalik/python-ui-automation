40ffd45a89333df0bbc168eaf6438dbd6d78fb09
Sneha Malik <sneha.malik@Brightlife.com>
2018-07-11 14:13:49 +0530

    not required

--- loggingpackage/logging_demo1.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-
-
-#logging.basicConfig(filename='test.log',level=logging.DEBUG)
-logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s',datefmt='%m/%d/%Y %I:%M:%S %p',level=logging.DEBUG)
-logging.warning("This is a warning msg")
-logging.info("This is a info msg")
-logging.error("Error message")
